ce07a22a35195388d00af0c260c854ece77c2a34
Dylan McReynolds <dmcreynolds@lbl.gov>
2024-03-06 09:04:13 -0800

    switched conda invoke methods

--- flows/conda/run_conda.sh
+++ flows/conda/run_conda.sh
@@ -11,13 +11,9 @@ conda_environment=$1
 python_file=$2
 yaml_file=$3
 
-# Activate the conda environment
-source activate $conda_environment
 
-echo Calling model with:    python $python_file $yaml_file
+
+echo Calling model with:  conda run -n $conda_environment $python_file $yaml_file
 
 # Call python with the python file and yaml file as arguments
-python $python_file $yaml_file
-
-# Deactivate the conda environment
-conda deactivate
+conda run -n $conda_environment $python_file $yaml_file
