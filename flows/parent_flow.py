ff22ba8f17fbddc8438865b930ea2488b8395ade
taxe10 <tanchavez@lbl.gov>
2024-03-06 13:00:48 -0800

    added conda flows as an option for parent flows

--- flows/parent_flow.py
+++ flows/parent_flow.py
@@ -3,11 +3,10 @@ from typing import Union
 
 from prefect import context, flow, get_run_logger
 
+from flows.conda.conda_flows import launch_conda
+from flows.conda.schema import CondaParams
 from flows.podman.podman_flows import launch_podman
 from flows.podman.schema import PodmanParams
-
-# from flows.conda.conda_flows import launch_conda
-# from flows.conda.schema import CondaParams
 
 
 class FlowType(str, Enum):
@@ -17,7 +16,8 @@ class FlowType(str, Enum):
 
 @flow(name="Parent flow")
 async def launch_parent_flow(
-    flow_type: FlowType, params_list: list[Union[PodmanParams]]  # , CondaParams]],
+    flow_type: FlowType,
+    params_list: list[Union[PodmanParams, CondaParams]],
 ):
     prefect_logger = get_run_logger()
     flow_run_id = context.get_run_context().flow_run.id
@@ -26,8 +26,7 @@ async def launch_parent_flow(
         if flow_type == FlowType.podman:
             await launch_podman(parent_run_id=flow_run_id, podman_params=params)
         elif flow_type == FlowType.conda:
-            pass
-            # await launch_conda(parent_run_id=flow_run_id, conda_params=params)
+            await launch_conda(parent_run_id=flow_run_id, conda_params=params)
         else:
             prefect_logger.error("Flow type not supported")
             raise ValueError("Flow type not supported")
