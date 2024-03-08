53af11696fea8df7da52d8dc3d9658971833f166
taxe10 <tanchavez@lbl.gov>
2024-03-08 00:10:11 -0800

    child flows store results according to their respective flow_run_ids instead of using parent flow_run_id

--- flows/parent_flow.py
+++ flows/parent_flow.py
@@ -1,7 +1,7 @@
 from enum import Enum
 from typing import Union
 
-from prefect import context, flow, get_run_logger
+from prefect import flow, get_run_logger
 
 from flows.conda.conda_flows import launch_conda
 from flows.conda.schema import CondaParams
@@ -20,13 +20,17 @@ async def launch_parent_flow(
     params_list: list[Union[PodmanParams, CondaParams]],
 ):
     prefect_logger = get_run_logger()
-    flow_run_id = context.get_run_context().flow_run.id
 
+    flow_run_id = None
     for params in params_list:
         if flow_type == FlowType.podman:
-            await launch_podman(parent_run_id=flow_run_id, podman_params=params)
+            flow_run_id = await launch_podman(
+                podman_params=params, prev_flow_run_id=flow_run_id
+            )
         elif flow_type == FlowType.conda:
-            await launch_conda(parent_run_id=flow_run_id, conda_params=params)
+            flow_run_id = await launch_conda(
+                conda_params=params, prev_flow_run_id=flow_run_id
+            )
         else:
             prefect_logger.error("Flow type not supported")
             raise ValueError("Flow type not supported")
