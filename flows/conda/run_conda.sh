7655b9fdd0925796cdbf1ae45f5f908ce1058ae3
Tanny Chavez <tanchavez@f8-ff-c2-23-6-51.dhcp.lbnl.us>
2024-03-25 17:19:00 -0700

    adding env variable for CONDA_PATH and added --no-capture-output to avoid buffered conda run

--- flows/conda/run_conda.sh
+++ flows/conda/run_conda.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+source .env
 
 # Check if all arguments are provided
 if [ $# -ne 3 ]; then
@@ -6,19 +7,16 @@ if [ $# -ne 3 ]; then
     exit 1
 fi
 
-CONDA_PATH="/scratch/dvp85458/miniconda"
 source "$CONDA_PATH/etc/profile.d/conda.sh"
 # Assign arguments to variables
 conda_environment=$1
 python_file=$2
 yaml_file=$3
 
-
-
 echo Calling model with:  conda run -n $conda_environment python $python_file $yaml_file
 
 # Call python with the python file and yaml file as arguments
-conda run -n $conda_environment python $python_file $yaml_file
+conda run --no-capture-output -n $conda_environment python $python_file $yaml_file
 
 # Check if the Conda command was successful
 if [ "$?" -ne 0 ]; then
