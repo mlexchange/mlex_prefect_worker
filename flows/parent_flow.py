a2cd6a145577203b1a1fa605b4b9460d8ab17e70
taxe10 <tanchavez@lbl.gov>
2024-04-02 07:57:28 -0700

    update parent flow + version to support slurm and add slurm to yaml file

--- flows/parent_flow.py
+++ flows/parent_flow.py
@@ -7,21 +7,24 @@ from flows.conda.conda_flows import launch_conda
 from flows.conda.schema import CondaParams
 from flows.podman.podman_flows import launch_podman
 from flows.podman.schema import PodmanParams
+from flows.slurm.schema import SlurmParams
+from flows.slurm.slurm_flows import launch_slurm
 
 
 class FlowType(str, Enum):
     podman = "podman"
     conda = "conda"
+    slurm = "slurm"
 
 
 @flow(name="Parent flow")
 async def launch_parent_flow(
     flow_type: FlowType,
-    params_list: list[Union[PodmanParams, CondaParams]],
+    params_list: list[Union[PodmanParams, CondaParams, SlurmParams]],
 ):
     prefect_logger = get_run_logger()
 
-    flow_run_id = ""
+    flow_run_id = None
     for params in params_list:
         if flow_type == FlowType.podman:
             flow_run_id = await launch_podman(
@@ -30,6 +33,10 @@ async def launch_parent_flow(
         elif flow_type == FlowType.conda:
             flow_run_id = await launch_conda(
                 conda_params=params, prev_flow_run_id=flow_run_id
+            )
+        elif flow_type == FlowType.slurm:
+            flow_run_id = await launch_slurm(
+                slurm_params=params, prev_flow_run_id=flow_run_id
             )
         else:
             prefect_logger.error("Flow type not supported")
